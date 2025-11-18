92e8174f6a7251a5259bb52d47e6069b51ac0c60
Eitan Turok <eitan.turok@gmail.com>
2025-11-18 15:32:12 +0200

    update

--- lab1/mandelbrot_cpu.cpp
+++ lab1/mandelbrot_cpu.cpp
@@ -13,11 +13,13 @@
 // Based on the "optimized escape time algorithm" in
 // https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
 void mandelbrot_cpu_scalar(uint32_t img_size, uint32_t max_iters, uint32_t *out) {
+    int VECTOR_SIZE = 16; // todo: remove
     for (uint64_t i = 0; i < img_size; ++i) {
         for (uint64_t j = 0; j < img_size; ++j) {
             // Get the plane coordinate X for the image pixel.
             float cx = (float(j) / float(img_size)) * 2.5f - 2.0f;
             float cy = (float(i) / float(img_size)) * 2.5f - 1.25f;
+            std::cout << "i=" <<i <<"\t\tj=" << j << "\n" << "cx=" << cx << "\t\tcy=" << cy << "\n"; // todo: remove
 
             // Innermost loop: start the recursion from z = 0.
             float x2 = 0.0f;
@@ -32,10 +34,24 @@ void mandelbrot_cpu_scalar(uint32_t img_size, uint32_t max_iters, uint32_t *out)
                 float z = x + y;
                 w = z * z;
                 ++iters;
+                // todo: remove
+                std::cout << "x=" << x << "\t\ty=" << y << "\n";
+                std::cout << "x2=" << x2 << "\t\ty2=" << y2 << "\n";
+                std::cout << "z=" << z << "\t\tw=" << w << "\n";
             }
+            std::cout << "iters=" << iters << "\n";
+
+            // if (j >= VECTOR_SIZE-1) { // todo: remove
+            //     break;
+            // }
 
             // Write result.
             out[i * img_size + j] = iters;
+
+            std::cout << "\n\n";
+        }
+        if (i >= 0) { // todo: remove
+            break;
         }
     }
 }
@@ -50,6 +66,169 @@ void print_m512(__m512 v) {
     std::cout << std::endl;
 }
 
+void print_m512i(__m512i v) {
+    int32_t arr[16];
+    _mm512_storeu_si512((__m512i*)arr, v);
+    for (int i = 0; i < 16; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+__mmask16 is_less_than_four(__m512 x2_vec, __m512 y2_vec, __mmask16 mask, __m512 zeros, __m512 fours) {
+    __m512 x_plus_y = _mm512_mask_add_ps(zeros, mask, x2_vec, y2_vec);
+    __mmask16 less_than_four = _mm512_mask_cmple_ps_mask(mask, x_plus_y, fours);
+    return less_than_four;
+}
+
+void mandelbrot_cpu_vector(uint32_t img_size, uint32_t max_iters, uint32_t *out) {
+    // check image fits
+    assert (img_size % 16 == 0);
+    int VECTOR_SIZE = 16;
+
+    // init values for outer loop
+    // for some reason you must input the arguments to set_ps backwards
+    __m512 range = _mm512_set_ps(15.0, 14.0, 13.0, 12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0);
+    __m512 img_size_vec = _mm512_set1_ps(img_size);
+    __m512 scale = _mm512_set1_ps(2.5);
+    __m512 x_shift = _mm512_set1_ps(2.0);
+    __m512 y_shift = _mm512_set1_ps(1.25);
+
+    __m512 i_vec;
+    __m512 j_vec;
+    __m512 cx_vec;
+    __m512 cy_vec;
+
+    // init values for inner loop
+    __m512 zeros = _mm512_set1_ps(0.0);
+    __m512 fours = _mm512_set1_ps(4.0);
+    __m512i ones = _mm512_set1_epi32(1);
+    __m512i max_iters_vec = _mm512_set1_epi32(max_iters);
+
+    bool do_continue;
+    __m512 x_vec;
+    __m512 y_vec;
+    __m512 x2_vec;
+    __m512 y2_vec;
+    __m512 w_vec;
+    __m512 z_vec;
+    __m512i iters_vec;
+
+    __mmask16 mask = 0b1111111111111111; // initially everything is True
+
+    // outer loop iterates over one 16-wide row of pixels at a time
+    for (uint64_t i = 0; i < img_size; i+=1) {
+        for (uint64_t j = 0; j < img_size; j+=VECTOR_SIZE) {
+            std::cout << "i=" <<i <<"j=" << j << "\n";
+            i_vec = _mm512_set1_ps(i);
+            j_vec = _mm512_add_ps(_mm512_set1_ps(j), range);
+            std::cout << "i_vec=";
+            print_m512(i_vec);
+            std::cout << "j_vec=";
+            print_m512(j_vec);
+
+            // get coordinate plane
+            // cx contain different values; cy repeats the same value
+            cx_vec = _mm512_sub_ps(_mm512_mul_ps(_mm512_div_ps(j_vec, img_size_vec), scale), x_shift);
+            cy_vec = _mm512_sub_ps(_mm512_mul_ps(_mm512_div_ps(i_vec, img_size_vec), scale), y_shift);
+
+            std::cout << "cx_vec=";
+            print_m512(cx_vec);
+            std::cout << "cy_vec=";
+            print_m512(cy_vec);
+            std::cout << "\n";
+
+            // set values for inner loop
+            x2_vec = _mm512_set1_ps(0.0);
+            y2_vec = _mm512_set1_ps(0.0);
+            w_vec = _mm512_set1_ps(0.0);
+            z_vec = _mm512_set1_ps(0.0);
+            zeros = _mm512_set1_ps(0.0);
+            iters_vec = _mm512_set1_epi32(0);
+            mask = 0b1111111111111111; // initially everything is True
+
+            // while loop condition
+            do_continue = (is_less_than_four(x2_vec, y2_vec, mask, zeros, fours) & _mm512_mask_cmplt_epi32_mask(mask, iters_vec, max_iters_vec)) != 0;
+            std::cout << "do_continue=" << do_continue << "\n";
+
+            while (do_continue) {
+                // compute x_vec, y_vec
+                x_vec = _mm512_mask_sub_ps(zeros, mask, x2_vec, y2_vec);
+                x_vec = _mm512_mask_add_ps(zeros, mask, x_vec, cx_vec);
+
+                y_vec = _mm512_mask_sub_ps(zeros, mask, w_vec, x2_vec);
+                y_vec = _mm512_mask_sub_ps(zeros, mask, y_vec, y2_vec);
+                y_vec = _mm512_mask_add_ps(zeros, mask, y_vec, cy_vec);
+
+                std::cout << "x_vec=";
+                print_m512(x_vec);
+                std::cout << "y_vec=";
+                print_m512(y_vec);
+
+                // compute x2_vec, y2_vec
+                x2_vec = _mm512_mask_mul_ps(zeros, mask, x_vec, x_vec);
+                y2_vec = _mm512_mask_mul_ps(zeros, mask, y_vec, y_vec);
+
+                std::cout << "x2_vec=";
+                print_m512(x2_vec);
+                std::cout << "y2_vec=";
+                print_m512(y2_vec);
+
+                // compute z_vec, w_vec
+                z_vec = _mm512_mask_add_ps(zeros, mask, x_vec, y_vec);
+                w_vec = _mm512_mask_mul_ps(zeros, mask, z_vec, z_vec);
+
+                std::cout << "z_vec=";
+                print_m512(z_vec);
+                std::cout << "w_vec=";
+                print_m512(w_vec);
+
+                // update iters, mask, do_continue
+                iters_vec = _mm512_mask_add_epi32(iters_vec, mask, iters_vec, ones);
+                std::cout << "iters_vec=";
+                print_m512i(iters_vec);
+                std::cout << "mask=" << mask << "\n";
+
+                mask = is_less_than_four(x2_vec, y2_vec, mask, zeros, fours);
+                do_continue = (mask & _mm512_mask_cmplt_epi32_mask(mask, iters_vec, max_iters_vec)) != 0;
+                std::cout << "new_mask=" << mask << "\t\tdo_continue=" << do_continue << "\n";
+            }
+            // break;
+            // _mm512_storeu_si512(&out)
+            std::cout << "\n\n";
+        }
+        break;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 bool while_cond(__m512 iters_vec, __m512 max_iters_vec, __m512 x2_vec, __m512 y2_vec, __mmask16 mask) {
     const int LT_OP = 1;
     const int LE_OP = 2;
@@ -62,73 +241,9 @@ bool while_cond(__m512 iters_vec, __m512 max_iters_vec, __m512 x2_vec, __m512 y2
     print_m512(my_sum);
     __mmask16 val3 = _mm512_mask_cmp_ps_mask(mask, my_sum, four_vec, LE_OP);
     std::cout << "val3 " << val3 << "\n";
-    __mmask16 mask = val1 & val3;
+    mask = val1 & val3;
     bool ret = mask != 0;
     return ret;
-}
-
-void mandelbrot_cpu_vector(uint32_t img_size, uint32_t max_iters, uint32_t *out) {
-    // check image fits
-    int VECTOR_SIZE = 16;
-    assert(img_size % VECTOR_SIZE == 0);
-
-    // constant for plane coordinate
-    __m512 img_size_vec = _mm512_set1_ps(float(img_size));
-    __m512 scale_vec = _mm512_set1_ps(2.5);
-    __m512 xshift_vec = _mm512_set1_ps(2.0);
-    __m512 yshift_vec = _mm512_set1_ps(1.25);
-
-    for (uint64_t i = 0; i < img_size; i+=VECTOR_SIZE) {
-        for (uint64_t j = 0; j < img_size; j+=VECTOR_SIZE) {
-            std::cout << "i=" << i <<"j=" << j << "\n";
-
-            // vectorized indices
-            __m512 i_vec = _mm512_set_ps(i+15.0, i+14.0, i+13.0, i+12.0, i+11.0, i+10.0, i+9.0, i+8.0, i+7.0, i+6.0, i+5.0, i+4.0, i+3.0, i+2.0, i+1.0, i+0.0);
-            __m512 j_vec = _mm512_set_ps(j+15.0, j+14.0, j+13.0, j+12.0, j+11.0, j+10.0, j+9.0, j+8.0, j+7.0, j+6.0, j+5.0, j+4.0, j+3.0, j+2.0, j+1.0, j+0.0);
-            print_m512(i_vec);
-            print_m512(j_vec);
-
-            // vectorized coordinate plane
-            __m512 cx_vec = _mm512_sub_ps(_mm512_mul_ps(_mm512_div_ps(i_vec, img_size_vec), scale_vec), xshift_vec);
-            __m512 cy_vec = _mm512_sub_ps(_mm512_mul_ps(_mm512_div_ps(j_vec, img_size_vec), scale_vec), yshift_vec);
-            print_m512(cx_vec);
-            print_m512(cy_vec);
-
-            // Innermost loop: start the recursion from z = 0.
-            __m512 x2_vec = _mm512_set1_ps(0.0);
-            __m512 y2_vec = _mm512_set1_ps(0.0);
-            __m512 w_vec = _mm512_set1_ps(0.0);
-            __m512 iters_vec = _mm512_set1_ps(0.0);
-            __m512 max_iters_vec = _mm512_set1_ps(max_iters);
-            __mmask16 mask = 0b1111111111111111; // initially everything is True; __mmask16 is an alias for unsigned short
-
-            bool cond = while_cond(iters_vec, max_iters_vec, x2_vec, y2_vec, mask);
-            // compute x
-            __m512 x_vec = _mm512_mask_sub_ps(iters_vec, mask, x2_vec, y2_vec);
-            __m512 x_vec = _mm512_mask_add_ps(iters_vec, mask, x_vec, cx_vec);
-
-            // compute y
-            __m512 y_vec = _mm512_mask_sub_ps(iters_vec, mask, w_vec, x2_vec);
-            __m512 y_vec = _mm512_mask_sub_ps(iters_vec, mask, y_vec, y2_vec);
-            __m512 y_vec = _mm512_mask_add_ps(iters_vec, mask, y_vec, cy_vec);
-
-
-            // while (_mm512_cmp_ps_mask(iters, max_iters_vec, LT_OP) != 0) {
-            //     __m512 x_vec =  _mm512_mask_sub_ps(src, mask, x2, y2)
-            //     float x = x2 - y2 + cx;
-            //     float y = w - x2 - y2 + cy;
-            //     x2 = x * x;
-            //     y2 = y * y;
-            //     float z = x + y;
-            //     w = z * z;
-            //     if x2 + y2 > 4.0f {
-            //         break
-            //     }
-            //     ++iters;
-            // }
-
-        }
-    }
 }
 
 /// <--- /your code here --->
@@ -273,7 +388,8 @@ std::vector<uint8_t> iters_to_colors(
 }
 
 // Benchmarking macros and configuration.
-static constexpr size_t kNumOfOuterIterations = 10;
+// static constexpr size_t kNumOfOuterIterations = 10;
+static constexpr size_t kNumOfOuterIterations = 1; // todo: remove
 static constexpr size_t kNumOfInnerIterations = 1;
 #define BENCHPRESS(func, ...) \
     do { \
@@ -324,7 +440,8 @@ void dump_image(
 int main(int argc, char *argv[]) {
     // Get Mandelbrot spec.
     uint32_t img_size = 256;
-    uint32_t max_iters = 1000;
+    // uint32_t max_iters = 1000;
+    uint32_t max_iters = 2; // todo: remove
     enum MandelbrotImpl impl = ALL;
     if (ParseArgsAndMakeSpec(argc, argv, &img_size, &max_iters, &impl))
         return -1;
